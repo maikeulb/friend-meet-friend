bbcba704962ab3de5e5fe7b06ed331dde6f259b4
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-26 02:16:50 -0400

    add db reads for users

--- app/users/data.go
+++ app/users/data.go
@@ -1,4 +1,4 @@
-package messages
+package users
 
 import (
 	"database/sql"
@@ -7,10 +7,10 @@ import (
 
 func (model *User) getUsers(db *sql.DB) (User, error) {
 	rows, err := db.Query(
-		`SELECT *
-        FROM messages
-        WHERE user_id = $1`,
-		m.userID)
+		`SELECT u.id, u.username, u.last_active, f.follower_id, f.followee_id
+        FROM users
+        INNER JOIN followings 
+        ON m.sender_id = u.id`)
 
 	if err != nil {
 		return nil, err
@@ -18,18 +18,17 @@ func (model *User) getUsers(db *sql.DB) (User, error) {
 
 	defer rows.Close()
 
-	messages := []Message{}
+	users := []User{}
 
 	for rows.Next() {
-		var m Message
+		var u User
 		if err := rows.Scan(
-			&m.ID,
-			&m.Body,
-			&m.RecipientID,
-			&m.SenderID); err != nil {
+			&u.ID,
+			&u.Username,
+			&u.LastActive); err != nil {
 			return nil, err
 		}
-		messages = append(messages, m)
+		users = append(users, u)
 	}
 
 	return messages, nil
@@ -37,13 +36,15 @@ func (model *User) getUsers(db *sql.DB) (User, error) {
 
 func (model *User) getProfile(db *sql.DB) (User, error) {
 	return db.QueryRow(
-		`SELECT * // join with followings
-                FROM messages
+		`SELECT u.id, u.username, u.last_active, u.bio, u.created_on, f.followr_id, f.followee_id
+                FROM users
+                INNER JOIN followings
                 WHERE id=$1`, m.ID).Scan(
-		&m.SenderID,
-		&m.RecipientID,
-		&m.Body,
-		&m.IsRead)
+		&m.ID,
+		&m.Username,
+		&m.Bio,
+		&m.CreatedOn,
+		&m.LastActive)
 }
 
 func (model *User) editProfile(db *sql.DB) (User, error) {
