a37e91b503a0d42b56f1e593806a232da9096aac
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-25 23:27:56 -0400

    initial schma

--- app/migrations/sql/V1_1__Initial_Schema.sql
+++ app/migrations/sql/V1_1__Initial_Schema.sql
@@ -1,34 +1,49 @@
-CREATE TABLE states (
-    id serial PRIMARY KEY,
-    name varchar(50) NULL
-);
-
 CREATE TABLE users (
     id serial PRIMARY KEY,
     username varchar(50) NOT NULL,
-    email TEXT NOT NULL,
-    password_hash varchar(50) NOT NULL,
-    password_salt varchar(50) NOT NULL,
-    bio integer NOT NULL,
-    created_on NOT NULL,
-    last_active NOT NULL,
-    last_message_read_time integer NULL
-    follower_id integer NULL
-    followed_id_ integer NULL
-    message_sent_id integer NULL
-    message_recieved_id integer NULL
+    email varchar(50) NOT NULL,
+    password_hash bytea NOT NULL,
+    password_salt bytea(50) NOT NULL,
+    bio TEXT NOT NULL,
+    created_on timestamp NOT NULL,
+    last_active timestamp NOT NULL
 );
 
 CREATE TABLE messages (
     id serial PRIMARY KEY,
     body integer NOT NULL,
-    timestamp integer NULL
+    timestamp timestamp NOT NULL,
     sender_id integer NOT NULL,
     recipient_id integer NULL
 );
 
 CREATE TABLE followings (
-    follower_id serial PRIMARY KEY,
-    followed_id integer NULL
+    follower_id integer NOT NULL,
+    followed_id integer NOT NULL,
+    PRIMARY KEY (follower_id, follower_id)
 );
+
+ALTER TABLE messages
+ADD CONSTRAINT fk_messages_senders
+FOREIGN KEY (sender_id)
+REFERENCES users(id)
+ON DELETE CASCADE;
+
+ALTER TABLE messages
+ADD CONSTRAINT fk_messages_recipients
+FOREIGN KEY (recipient_id)
+REFERENCES users(id)
+ON DELETE CASCADE;
+
+ALTER TABLE followings
+ADD CONSTRAINT fk_followers_users
+FOREIGN KEY (follower_id)
+REFERENCES users(id)
+ON DELETE CASCADE;
+
+ALTER TABLE followings
+ADD CONSTRAINT fk_followed_users
+FOREIGN KEY (followed_id)
+REFERENCES users(id)
+ON DELETE CASCADE;
 
