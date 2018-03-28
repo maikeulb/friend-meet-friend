3075e59f3c39409c2eaa67bf232bcf37b9f088a2
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-03-28 18:57:13 -0400

    add cookie authentication

--- app/app.go
+++ app/app.go
@@ -1,10 +1,12 @@
 package app
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/maikeulb/friend-meet-friend/app/messages"
@@ -32,6 +34,8 @@ func (a *App) Initialize(host, port, user, password, dbname string) {
 
 func (a *App) Run(addr string) {
 	fmt.Println("Listening on port: 5000")
+	fmt.Println("/api/login")
+	fmt.Println("/api/status")
 	fmt.Println("/api/messages/{id}")
 	fmt.Println("/api/messages/sent")
 	fmt.Println("/api/messages/received")
@@ -39,9 +43,12 @@ func (a *App) Run(addr string) {
 }
 
 func (a *App) initializeRoutes() {
+	a.Router.HandleFunc("/api/login", a.Login)
+	a.Router.HandleFunc("/api/status", a.Status)
 	a.Router.HandleFunc("/api/messages/{id:[0-9]+}", a.GetMessage).Methods("GET")
 	a.Router.HandleFunc("/api/messages/sent", a.GetSentMessages).Methods("GET")
 	a.Router.HandleFunc("/api/messages/recieved", a.GetRecievedMessages).Methods("GET")
+	a.Router.Use(a.AddContextMiddleware)
 }
 
 func (a *App) GetSentMessages(w http.ResponseWriter, r *http.Request) {
@@ -54,4 +61,34 @@ func (a *App) GetRecievedMessages(w http.ResponseWriter, r *http.Request) {
 
 func (a *App) GetMessage(w http.ResponseWriter, r *http.Request) {
 	messages.GetMessage(a.DB, w, r)
+}
+
+func (a *App) AddContextMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, "-", r.RequestURI)
+		cookie, _ := r.Cookie("username")
+		if cookie != nil {
+			//Add data to context
+			ctx := context.WithValue(r.Context(), "Username", cookie.Value)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		} else {
+			next.ServeHTTP(w, r)
+		}
+	})
+}
+
+func (a *App) Login(w http.ResponseWriter, r *http.Request) {
+	expiration := time.Now().Add(365 * 24 * time.Hour)
+	cookie := http.Cookie{Name: "username", Value: "demo@gmail.com", Expires: expiration}
+	http.SetCookie(w, &cookie)
+}
+
+func (a *App) Status(w http.ResponseWriter, r *http.Request) {
+	if username := r.Context().Value("Username"); username != nil {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("Hello " + username.(string) + "\n"))
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Not Logged in"))
+	}
 }
